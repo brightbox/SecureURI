8e91ae0e1c4990ae1e7e0437f187f34244574d81
Caius Durling <dev@caius.name>
2009-12-03 19:54:54 +0000

    Replacing bcrypt with SHA256, because bcrypt only works on strings 72 characters or less.

--- lib/secure_uri.rb
+++ lib/secure_uri.rb
@@ -1,6 +1,7 @@
 require "uri"
-%w(hasher secure_uri bcrypt_hasher).each do |file|
+%w(secure_uri hasher sha256_hasher).each do |file|
   require File.join(File.dirname(__FILE__), "secure_uri", file)
 end
 
+# All specific classes of URI are subclasses of Generic
 URI::Generic.__send__(:include, SecureURI)
